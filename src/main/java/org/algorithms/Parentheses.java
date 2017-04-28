547e5cdddb9f283197fc464740b8c41313f320e0
vmayorov <vmayorov@nxsystemsinc.com>
2017-04-28 18:35:47 +0300

    added parentheses fix

--- src/main/java/org/algorithms/Parentheses.java
+++ src/main/java/org/algorithms/Parentheses.java
@@ -7,12 +7,12 @@ package org.algorithms;
 import org.algorithms.data.LinkedStack;
 import org.algorithms.data.Stack;
 
-/**
- *  1.3.4 Write a stack client Parentheses that reads in a text stream from standard input
- and uses a stack to determine whether its parentheses are properly balanced.
- For example, your program should print true for [()]{}{[()()]()} and false for [(]).
- */
 public class Parentheses {
+    /**
+     *  1.3.4 Write a stack client Parentheses that reads in a text stream from standard input
+     and uses a stack to determine whether its parentheses are properly balanced.
+     For example, your program should print true for [()]{}{[()()]()} and false for [(]).
+     */
     public boolean isBalanced(String expr) {
         Stack<Character> stack = new LinkedStack<>();
         int count = 0;
@@ -42,5 +42,32 @@ public class Parentheses {
         }
 
         return stack.isEmpty() && count == 0;
+    }
+
+    /**
+     * 1.3.9 Write a program that takes from standard input an expression without left pa- rentheses and prints the equivalent infix expression with the parentheses inserted. For example, given the input:
+     1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )
+     your program should print
+     ( ( 1 + 2 ) * ( ( 3 -4 ) * ( 5 - 6 ) )
+
+     */
+    public String fix(String expr) {
+        Stack<String> stack = new LinkedStack<>();
+
+        for (String item : expr.split(" ")) {
+            if (item.equals(")")) {
+                String exp1 = stack.pop();
+                String exp2 = stack.pop();
+                String exp3 = stack.pop();
+
+                stack.push("( " + exp3 + " " + exp2 + " " + exp1 + " )");
+            } else {
+                stack.push(item);
+            }
+        }
+
+        assert stack.size() == 1;
+
+        return stack.pop();
     }
 }
--- src/test/java/org/algorithms/ParenthesesTest.java
+++ src/test/java/org/algorithms/ParenthesesTest.java
@@ -7,10 +7,16 @@ import static org.junit.Assert.*;
 public class ParenthesesTest {
 
     @Test
-    public void test() {
+    public void isBalanced() {
         Parentheses p = new Parentheses();
         assertTrue(p.isBalanced("()"));
         assertTrue(p.isBalanced("[()]{}{[()()]()}"));
         assertFalse(p.isBalanced("[(])"));
+    }
+
+    @Test
+    public void fix() {
+        Parentheses p = new Parentheses();
+        assertEquals("( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )", p.fix("1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )"));
     }
 }
